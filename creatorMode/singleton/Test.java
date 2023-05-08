af5cd08a75f27b3bee16714097a1464a55f2907f
wanghao <947167041@qq.com>
2023-05-08 11:16:36 +0800

    单例模式

---
+++ creatorMode/singleton/Test.java
@@ -0,0 +1,13 @@
+package singleton;
+
+/**
+ * @Title: Test
+ * @Author bubuwang
+ * @Date 2023/5/8 11:16
+ * @description: 测试类
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
