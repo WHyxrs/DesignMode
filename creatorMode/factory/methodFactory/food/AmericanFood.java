bf54b2639cbdb9c00fc5d2329514837f7e8b1b0e
wanghao <947167041@qq.com>
2023-05-11 17:39:25 +0800

    美国食物接口

---
+++ creatorMode/factory/methodFactory/food/AmericanFood.java
@@ -0,0 +1,11 @@
+package factory.methodFactory.food;
+
+/**
+ * @Title: AmericanFood
+ * @Author bubuwang
+ * @Date 2023/5/11 16:35
+ * @description: 美国食物
+ */
+public interface AmericanFood extends Food {
+    public void food();
+}
