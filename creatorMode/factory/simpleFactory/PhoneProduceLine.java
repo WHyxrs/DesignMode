b4627c164125603b71c2e4d84b59c1e9cb4b6b68
wanghao <947167041@qq.com>
2023-05-11 16:21:53 +0800

    生产线接口

---
+++ creatorMode/factory/simpleFactory/PhoneProduceLine.java
@@ -0,0 +1,14 @@
+package factory.simpleFactory;
+
+/**
+ * @Title: MobilePhone
+ * @Author bubuwang
+ * @Date 2023/5/11 15:56
+ * @description: 父接口
+ */
+public interface PhoneProduceLine {
+    /**
+     * 创造手机
+     */
+    public void create();
+}
