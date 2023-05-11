19f2d9ec374ca99f73786bd864f14bf2fbfd511b
wanghao <947167041@qq.com>
2023-05-11 17:38:04 +0800

    食物 顶级父接口

---
+++ creatorMode/factory/methodFactory/food/Food.java
@@ -0,0 +1,11 @@
+package factory.methodFactory.food;
+
+/**
+ * @Title: Food
+ * @Author bubuwang
+ * @Date 2023/5/11 16:33
+ * @description: 食物接口
+ */
+public interface Food {
+    public void food();
+}
