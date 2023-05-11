3f3567ca70fb4dbe65aac280ea5295fc4ea20ef0
wanghao <947167041@qq.com>
2023-05-11 17:41:00 +0800

    美国食物类

---
+++ creatorMode/factory/methodFactory/food/AmericanFoodA.java
@@ -0,0 +1,14 @@
+package factory.methodFactory.food;
+
+/**
+ * @Title: AmericanFoodA
+ * @Author bubuwang
+ * @Date 2023/5/11 16:42
+ * @description: 美国食物A
+ */
+public class AmericanFoodA implements AmericanFood{
+    @Override
+    public void food() {
+        System.out.println("美国食物A");
+    }
+}
---
+++ creatorMode/factory/methodFactory/food/AmericanFoodB.java
@@ -0,0 +1,15 @@
+package factory.methodFactory.food;
+
+/**
+ * @Title: AmericanFoodB
+ * @Author bubuwang
+ * @Date 2023/5/11 16:42
+ * @description: 美国食物B
+ */
+public class AmericanFoodB implements AmericanFood{
+
+    @Override
+    public void food() {
+        System.out.println("美国食物B");
+    }
+}
