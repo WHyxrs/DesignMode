24008b24bbb4b3e1abb82554a1c05b5968b10d96
wanghao <947167041@qq.com>
2023-05-11 17:38:43 +0800

    中国食物接口

---
+++ creatorMode/factory/methodFactory/food/ChineseFood.java
@@ -0,0 +1,11 @@
+package factory.methodFactory.food;
+
+/**
+ * @Title: ChineseFood
+ * @Author bubuwang
+ * @Date 2023/5/11 16:34
+ * @description: 中国食物
+ */
+public interface ChineseFood extends Food {
+
+}
